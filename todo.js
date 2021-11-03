d85cfc352c4c2d8d5431be7f9ee2b6c8c5534139
tujianhao <1149858111@qq.com>
2021-11-03 17:48:40 +0800

    一次更新👨‍💻

--- todo.js
+++ todo.js
@@ -7,3 +7,11 @@
 * 目前
 	* 看完《你不知道的JS》
 	* js忍者秘籍
+
+* 汇总
+	* daily 集团死亡总数对比 接口数据无法进行图表转换。
+	* subsidiary 方舱各工段周死亡头数以及死亡率 无数据
+	* subsidiary 子公司各场区日死亡情况接口500  getFieldDeadthFun
+	* testingground 试验场方舱批次周死亡情况 无数据 provingGroundWeekDeadthCondition
+	* testingground 验场日死亡头数前5的批次 无数据 provingGroundYesterdayDeadthHeads
+	* testingground 试验场架构下周死亡率前5批次 500 provingGroundWeekDeadthRate
